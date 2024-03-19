fde9900791bf70d17d1a333478b0f7190c3732b3
Lam Nguyen <tunglam030699@gmail.com>
2024-03-19 15:33:19 -0400

    first commit

---
+++ app/board/[boardId]/page.tsx
@@ -0,0 +1,18 @@
+import { Canvas } from "./_components/canvas";
+import { Room } from "@/components/room";
+
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+}
+
+const BoardIdPage = ({ params }: BoardIdPageProps) => {
+  return (
+    <Room roomId={params.boardId}>
+      <Canvas boardId={params.boardId} />
+    </Room>
+  );
+};
+
+export default BoardIdPage;
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://magical-elk-87.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
